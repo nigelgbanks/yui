76e2956e42bc05e21b780a2be5d88c5b632dc33b
Nigel Banks <nigel.g.banks@gmail.com>
2012-06-27 11:18:04 -0300

    Now supports multiple submits when vaildation for a different element is present.

--- modules/uploader/js/yui-drupal-uploader.js
+++ modules/uploader/js/yui-drupal-uploader.js
@@ -1,12 +1,32 @@
 Drupal.behaviors.yuiUploader = function() {
   if (typeof arguments.callee.called == 'undefined') {
     arguments.callee.called = true;
-    YUI().use('uploader', 'uploader-flash', function (Y) {
+    YUI().use('uploader-flash', function (Y) {
       if(Y.Uploader.TYPE == "none" || Y.UA.ios) {
         Y.one(Drupal.settings.yui.uploader.select).set("text", "We are sorry, but the uploader technology is not supported on this platform.");
         return;
       }
       var uploader = new Y.UploaderFlash(Drupal.settings.yui.uploader.config);
+      var uploaded = false;
+      var upload_required = Drupal.settings.yui.uploader.required;
+      if(Drupal.settings.yui.uploader.required) { // Upload required.
+        $('#' + Drupal.settings.yui.uploader.formID + ' input[type="submit"]').not('.ahah-processed').click(function(e) {
+          if(uploaded == false) {
+            e.preventDefault();
+            alert(Drupal.t('You must upload one or files, before you can submit the form.'));
+          }
+        });
+      }
+      if(Drupal.settings.yui.uploader.files.length > 0) {
+        Y.one("#yui-uploader-files").setStyle('display', 'block');
+        var file_table = Y.one("#yui-uploader-filenames tbody");
+        Y.each(Drupal.settings.yui.uploader.files, function (file) {
+          file_table.append("<tr>" +
+                            "<td class='filename'>" + file.filename + "</td>" +
+                            "<td class='filesize'>" + file.filesize + "</td>" +
+                            "<td class='percentdone'>Upload complete!</td></tr>");
+        });
+      }
       uploader.render(Drupal.settings.yui.uploader.select);
       uploader.after("fileselect", function (event) {
         var file_list = event.fileList;
@@ -15,7 +35,7 @@ Drupal.behaviors.yuiUploader = function() {
           file_table.append("<tr id='" + file_instance.get("id") + "_row" + "'>" +
                             "<td class='filename'>" + file_instance.get("name") + "</td>" +
                             "<td class='filesize'>" + file_instance.get("size") + "</td>" +
-                            "<td class='percentdone'>Hasn't started yet</td>");
+                            "<td class='percentdone'>Hasn't started yet</td></tr>");
         });
         if (file_list.length > 0) {
           Y.one("#yui-uploader-files").setStyle('display', 'block');
@@ -38,7 +58,8 @@ Drupal.behaviors.yuiUploader = function() {
       });
       uploader.on("alluploadscomplete", function (event) {
         uploader.set("enabled", true);
-        Y.one("#yui-uploader-overall-progress").set("text", "Uploads complete!");
+        uploaded = true;
+        Y.one("#yui-uploader-overall-progress").set("text", "Upload complete!");
       });
     });
   }
